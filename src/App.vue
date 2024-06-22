ec8b4517da3783e7e4caf958973fc44746547b0f
ange <angekouamalan27@gmail.com>
2024-06-22 14:28:55 +0000

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
     <main>
-        <!-- <Add_item @ajouter_liste="onInput" /> -->
          <Watchers @users="watchers_data"/>
     </main>
 </template>
 
 <script setup>
-import Add_item from "@/components/Add_item.vue";
 import Watchers from "./components/Watchers.vue";
-import { ref } from "vue";
-const users_data = ref([]);
-
-const watchers_data = (event) => {
-    users_data.value.push(event)
-} 
 
 </script>
 
